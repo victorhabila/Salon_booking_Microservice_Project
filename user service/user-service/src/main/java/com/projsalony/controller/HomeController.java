53f6230fb75e8a579fcc155b381f9a5fed4d560a
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-04-25 18:31:02 +0200

    User controller, repository abd Global exception handling

---
+++ user service/user-service/src/main/java/com/projsalony/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.projsalony.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "User microservice for salon booking system";
+    }
+}
---
+++ user service/user-service/src/main/java/com/projsalony/payload/response/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package com.projsalony.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ExceptionResponse {
+
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+}
