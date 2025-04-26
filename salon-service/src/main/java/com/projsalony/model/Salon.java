fff2ef0042667e34f7f3c6660ca7bbb83f180ac4
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-04-26 15:59:22 +0200

    Salon service, repository and model

---
+++ salon-service/src/main/java/com/projsalony/model/Salon.java
@@ -0,0 +1,43 @@
+package com.projsalony.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Salon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId; //owner of the salon
+
+    @Column(nullable = false)
+    private LocalTime openTime;
+
+    @Column(nullable = false)
+    private LocalTime closeTime;
+}
