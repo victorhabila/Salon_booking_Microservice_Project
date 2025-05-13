148987e60a6a7d4da7074772e28744fafd87449c
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-05-13 16:19:38 +0200

    Payment microservice project setup

---
+++ payment-service/src/main/java/com/salonyapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.salonyapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "payment microservice for salon booking system";
+    }
+}
