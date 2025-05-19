1b888f132b9f93104cc968c65e6770360817ab8f
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-05-19 21:21:20 +0200

    Payment service implementation

---
+++ payment-service/src/main/java/com/salonyapp/domain/BookingStatus.java
@@ -0,0 +1,8 @@
+package com.salonyapp.domain;
+
+public enum BookingStatus {
+
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
---
+++ payment-service/src/main/java/com/salonyapp/domain/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.salonyapp.domain;
+
+public enum PaymentMethod {
+    RAZORPAY,
+    STRIPE
+}
