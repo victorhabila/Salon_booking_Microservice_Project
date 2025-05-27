c23c627958920283019e2b95d3b860200c4235f3
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-05-27 19:52:39 +0200

    Payment microservice

---
+++ payment-service/src/main/java/com/projsalony/domain/PaymentOrderStatus.java
@@ -0,0 +1,7 @@
+package com.salonyapp.domain;
+
+public enum PaymentOrderStatus {
+    PENDING,
+    SUCCESS,
+    FAILED
+}
---
+++ payment-service/src/main/java/com/projsalony/repository/PaymentOrderRepository.java
@@ -0,0 +1,9 @@
+package com.salonyapp.repository;
+
+import com.salonyapp.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+
+    PaymentOrder findByPaymentLinkId(String paymentLinkId);
+}
