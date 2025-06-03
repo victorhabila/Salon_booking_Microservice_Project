7bb1214220a8eed9a678456c7d6f8bed0d2403e3
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-06-03 16:39:58 +0200

    Gate-way configuration

--- salon-service/src/main/java/com/projsalony/controller/SalonController.java
+++ salon-service/src/main/java/com/projsalony/controller/SalonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/salon")
+@RequestMapping("/api/salons")
 public class SalonController {
 
     private final SalonService salonService;
